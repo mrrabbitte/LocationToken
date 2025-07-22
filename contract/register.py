b1bb0774c13ae9a13e2a5954bc971eb74d303eda
Stefan Gliniecki <stgli@pm.me>
2025-07-22 16:31:57 +0100

    Register PoL function call

--- contract/register.py
+++ contract/register.py
@@ -3,6 +3,9 @@ import json
 from cryptography.hazmat.primitives import serialization
 from result import Result, Ok
 from web3.auto import w3
+from web3.types import TxParams
+
+from proof.challenger import ProofOfLocation
 
 
 class LocationTokenRegister:
@@ -25,3 +28,16 @@ class LocationTokenRegister:
             encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.SubjectPublicKeyInfo).decode()
         return Ok(pub_key)
+
+    def register_proof_of_location(self, proof: ProofOfLocation, tx: TxParams):
+        response = self.contract.functions.registerLocationProof(
+            travellerId=proof.traveller_id,
+            challengerId=proof.challenger_id,
+            nonce_c=proof.nonce_c,
+            created_at=proof.created_at,
+            ttl=proof.ttl,
+            c_signature=bytes.fromhex(proof.c_signature),
+            t_signature=bytes.fromhex(proof.t_signature),
+            proof=bytes.fromhex(proof.proof)
+        ).transact(tx)
+        print(response)
